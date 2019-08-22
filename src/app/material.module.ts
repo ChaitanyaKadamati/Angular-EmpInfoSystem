21349c06e630717617a4918228aec214db5e974a
ChaitanyaKadamati <kskchaitanya@gmail.com>
2019-08-23 00:19:09 +0530

    4 files modified

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,7 +3,9 @@ import {NgModule} from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
-const requiredModules = [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule];
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const requiredModules = [MatDatepickerModule, MatFormFieldModule, MatNativeDateModule, BrowserAnimationsModule];
 
 @NgModule({
   imports: [requiredModules],
