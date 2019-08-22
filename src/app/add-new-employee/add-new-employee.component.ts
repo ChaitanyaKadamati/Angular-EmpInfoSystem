9a9d02d8d7d0de751cf4a3e6bc027268500ec85a
ChaitanyaKadamati <kskchaitanya@gmail.com>
2019-08-22 22:17:33 +0530

    7 files modified

--- src/app/add-new-employee/add-new-employee.component.ts
+++ src/app/add-new-employee/add-new-employee.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new-employee.component.css']
 })
 export class AddNewEmployeeComponent implements OnInit {
+  inupdatemode = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+onSubmitData(formData) {
+  console.log(formData.value);
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AddNewEmployeeComponent } from './add-new-employee/add-new-employee.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
