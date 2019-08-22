fa2fd7731ef210152378dd29b157ad60a1c87f04
ChaitanyaKadamati <kskchaitanya@gmail.com>
2019-08-22 23:46:44 +0530

    2 files modified

--- src/app/add-new-employee/add-new-employee.component.ts
+++ src/app/add-new-employee/add-new-employee.component.ts
@@ -9,6 +9,7 @@ import { EmployeeService, Employee } from '../employee.service';
   styleUrls: ['./add-new-employee.component.css']
 })
 export class AddNewEmployeeComponent implements OnInit {
+  employee: Employee = null;
   inupdatemode = false;
   employeeId = null;
   constructor(private route: ActivatedRoute, private employeeService: EmployeeService) { }
@@ -22,6 +23,7 @@ export class AddNewEmployeeComponent implements OnInit {
         if (selectedEmployeeID == -1) {
           this.inupdatemode = false;
           this.employee = new Employee();
+          this.employee.hiredate = new Date();
         } else {
           this.inupdatemode = true;
           this.employeeId = selectedEmployeeID;
@@ -30,7 +32,7 @@ export class AddNewEmployeeComponent implements OnInit {
       }
     );
   }
-  employee: Employee = null;
+
   readEmployeeDeatils(paramEmpId) {
     this.employeeService.getEmployee(paramEmpId).subscribe(
       (res) => {
@@ -46,6 +48,13 @@ export class AddNewEmployeeComponent implements OnInit {
   }
 
   onSubmitData(formData) {
+    if (this.inupdatemode) {
+      this.employeeService.updateEmployee(this.employee);
+    } else {
+      this.employeeService.insertEmployee(this.employee);
+    }
+
     console.log(formData.value);
+    console.log(this.employee.mgrid);
   }
 }
