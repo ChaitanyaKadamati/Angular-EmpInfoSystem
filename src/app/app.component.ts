f9b624d426ca470cab7a9211bc05aca66bd701d3
ChaitanyaKadamati <kskchaitanya@gmail.com>
2019-08-28 17:01:58 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthenticationService } from '../auth/authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Employee Information Management';
   version = '1.0.31';
+  isUserAuthorized = false;
+
+  constructor(private authService: AuthenticationService) { }
+
+  ngOnInit() {
+    this.isUserAuthorized = this.authService.isUserAuthenticated();
+  }
 }
