3fb50744aed9e5ef54dede692a04c965a0286423
ChaitanyaKadamati <kskchaitanya@gmail.com>
2019-08-28 18:43:11 +0530

    3 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { AuthenticationService } from '../auth/authentication.service';
+;
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-  
-  constructor() { }
+
+  constructor(private authService: AuthenticationService, private router: Router) { }
+
+  logout() {
+    console.log('Logout');
+    this.authService.logout();
+    this.router.navigate(['']);
+  }
 }
